08ca246e4b6688d73ff5f528b3e400aa96b10759
Baglan Dosmagambetov <baglan.dos@gmail.com>
2012-07-20 20:43:46 +0600

    Initial Commit

---
+++ MCPagerView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCPagerView
+//
+//  Created by Baglan on 7/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
